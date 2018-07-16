931f50eb6222f5731ddea763bff860769bd8f49c
thefifthisa <ijlavandero@gmail.com>
2018-07-16 18:14:50 -0400

    day 4

---
+++ adventofcode/2017/day4_highEntropyPassphrases.py
@@ -0,0 +1,18 @@
+def passphrases_noDuplicates(list):
+    count = 0
+    for pp in list:
+        if len(set(pp.split())) == len(pp.split()): # sets have no duplicates
+            count += 1
+    return count
+
+def passphrases_noAnagrams(list):
+    count = 0
+    for pp in list:
+        if len(set([''.join(sorted(str)) for str in pp.split()])) == len(pp.split()): # sort each string in the list first to undo anagrams
+            count += 1
+    return count
+
+with open('day4_input.txt', 'r') as input:
+    list = input.readlines()
+    print(passphrases_noDuplicates(list))
+    print(passphrases_noAnagrams(list))
